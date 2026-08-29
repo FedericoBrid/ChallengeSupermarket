42f47281443f162b1581e0cd20e386544c84d0e6
Federico Brid <federicobrid@gmail.com>
2026-08-28 21:47:50 -0300

    add new project

---
+++ src/main/java/com/ChallengeSupermarket/ChallengeSupermarket/ChallengeSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.ChallengeSupermarket.ChallengeSupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChallengeSupermarket/ChallengeSupermarket/ChallengeSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChallengeSupermarket.ChallengeSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
